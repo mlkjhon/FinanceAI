5c40e0a43757e8578135bf5043b688f6e700dbea
jhonatan <jhonatan.moraes957@gmail.com>
2026-06-12 12:21:48 -0300

    fix: add dotenv configuration to load environment variables

--- api/app.js
+++ api/app.js
@@ -1,3 +1,6 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
 import express from 'express';
 import rotasUsuarios from "./src/routes/rotasUsuarios.js";
 import rotasTransacoes from "./src/routes/rotasTransacoes.js";
